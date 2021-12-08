b71190aeee4d30fce1870312acc4408c4d21c530
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-12-08 11:21:23 -0500

    Add lose screen and save high score locally

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ var s;
 var scl = 20;
 // food variable
 var food;
+var sceneNum = 0;
+// keep track of the high score
+var highScore;
 
 function setup() {
   // set the canvas to 600px by 600px
@@ -14,6 +17,8 @@ function setup() {
   frameRate(10);
   // call the pickLocation function
   pickLocation();
+  // recall the high score
+  highScore = getItem('highScore');
 }
 
 // Pick a location for the piece of food
@@ -31,36 +36,62 @@ function pickLocation() {
 function draw() {
   // set the background
   background(51);
+  textSize(18);
+  fill(255, 255, 255);
+  text("Score: " + s.total, 10, 30);
+  text("High Score: " + highScore, 10, 50);
   // if the snake's position is the same as the food's location
   if (s.eat(food)) {
     // pick a new food location
     pickLocation(); 
   }
-  // check if the snake has died
-  s.death();
-  // update the position of the snake
-  s.update();
-  // show the snake
-  s.show();
+  
+  
+  
+  if (sceneNum === 0) {
+    // check if the snake has died
+    s.death();
+    // update the position of the snake
+    s.update();
+    // show the snake
+    s.show();
+  } 
+  
   
   // display the food at the location chosen by the pickLocation function
   fill(255, 0, 100);
   rect(food.x, food.y, scl, scl);
+  
+  // show the score AFTER displaying the food so that the rectangle is drawn over the food
+  if (sceneNum === 1) {
+    s.showScore();
+  }
+    
 }
 
 // set keyboard controls
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    // set the direction to up
+  
+  if (keyCode === UP_ARROW && s.yspeed != 1) {
+    // set the direction to up ONLY if the snake is not going down
     s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    // set direction to down
+  } else if (keyCode === DOWN_ARROW && s.yspeed != -1) {
+    // set direction to down ONLY if the snake is not going up
     s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    // set direction to right
+  } else if (keyCode === RIGHT_ARROW && s.xspeed != -1) {
+    // set direction to right ONLY if the snake is not going left
     s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    // set direction to left
+  } else if (keyCode === LEFT_ARROW && s.xspeed != 1) {
+    // set direction to left ONLY if the snake is not going right
     s.dir(-1, 0);
   }
+  
 }
+
+function mousePressed() {
+  if (sceneNum === 1) {
+    sceneNum = 0;
+  }
+
+}
+
